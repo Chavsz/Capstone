d84116ce600a856558e4225942a73c8d4e910ae3
Chavy Tapuyao <martinchavy383@gmail.com>
2025-06-28 21:20:37 +0800

    change bg colors in landing page

--- frontend/src/landing pages/About.jsx
+++ frontend/src/landing pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <section id="about" className="min-h-screen flex items-center justify-center bg-white">
+    <section id="about" className="min-h-screen flex items-center justify-center bg-[#4766f1]">
       <h1 className="text-3xl font-bold">This is the About page</h1>
     </section>
   )
--- frontend/src/landing pages/Home.jsx
+++ frontend/src/landing pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <section id="Home" className="min-h-screen flex items-center justify-center bg-gray-100">
+    <section id="Home" className="min-h-screen flex items-center justify-center bg-[#ffffff]">
       <h1 className="text-3xl font-bold">This is the Home page</h1>
     </section>
   )
