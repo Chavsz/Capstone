d4ce120f9b42800ab7a3048537ffbe010638f465
Chavy Tapuyao <martinchavy383@gmail.com>
2025-06-28 21:49:51 +0800

    change bg colors

--- frontend/src/landing pages/Events.jsx
+++ frontend/src/landing pages/Events.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Events() {
   return (
-    <section id="events" className="min-h-screen flex items-center justify-center bg-gray-100">
+    <section id="events" className="min-h-screen flex items-center justify-center bg-[#feda3c]">
       <h1 className="text-3xl font-bold">This is the Events page</h1>
     </section>
   )
