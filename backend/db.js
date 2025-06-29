3880131beba5acab707892b5113b0c098fbdde98
Chavy Tapuyao <martinchavy383@gmail.com>
2025-06-29 14:21:27 +0800

    fix .env

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
+require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "chavy123", //Use your postgres password
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_DATABASE || "jwt_auth_multiuser"
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, //Use your postgres password
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT, 
+  database: process.env.DB_DATABASE, 
 });
 
 module.exports = pool;
